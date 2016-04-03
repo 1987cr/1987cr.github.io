d4018ee88c72d3a39378afed09f5d8c670b34003
Carlos Riera <cr.exe@hotmail.com>
2016-04-03 13:15:31 -0430

    6

--- scripts/main.js
+++ scripts/main.js
@@ -53,3 +53,31 @@ $('#host2').frameCarousel({
     'images/sa4.png',
     ]
 });
+
+$('#host3').frameCarousel({
+  debug: false,
+  first: 0,
+  swipeThreshold: 10,
+  frame: 'images/frame.png',
+  frameSize:[
+				{width: 600, minScreenWidth: 1500},
+				{width: 500, minScreenWidth: 1200, maxScreenWidth: 1499},
+				{width: 450, minScreenWidth: 992, maxScreenWidth: 1199},
+				{width: 300, minScreenWidth: 768, maxScreenWidth: 991},
+				{width: 450, minScreenWidth: 521, maxScreenWidth: 767},
+				{width: 300, minScreenWidth: 369, maxScreenWidth: 520},
+				{width: 250, maxScreenWidth: 368}
+          	],
+  collapseThreshold: 320,
+  boundingBox: {
+    left: '4.3%',
+    top: '4.48%',
+    width: '91.4%',
+    height: '61.1%'
+  },
+  images: [
+  	'images/emg1.png',
+    'images/emg3.png',
+    'images/emg2.png',
+    ]
+});
