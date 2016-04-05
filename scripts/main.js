aef7b2db5dd9fd84a63765f0fdd33a2737615e5d
Carlos Riera <cr.exe@hotmail.com>
2016-04-04 20:34:36 -0430

    La Montserratina

--- scripts/main.js
+++ scripts/main.js
@@ -110,6 +110,64 @@ $('#host4').frameCarousel({
     ]
 });
 
+$('#host5').frameCarousel({
+  debug: false,
+  first: 0,
+  swipeThreshold: 10,
+  frame: 'images/frame.png',
+  frameSize:[
+        {width: 600, minScreenWidth: 1500},
+        {width: 500, minScreenWidth: 1200, maxScreenWidth: 1499},
+        {width: 450, minScreenWidth: 992, maxScreenWidth: 1199},
+        {width: 300, minScreenWidth: 768, maxScreenWidth: 991},
+        {width: 450, minScreenWidth: 521, maxScreenWidth: 767},
+        {width: 300, minScreenWidth: 369, maxScreenWidth: 520},
+        {width: 250, maxScreenWidth: 368}
+            ],
+  collapseThreshold: 320,
+  boundingBox: {
+    left: '4.3%',
+    top: '4.48%',
+    width: '91.4%',
+    height: '61.1%'
+  },
+  images: [
+    'images/tn1.png',
+    'images/tn2.png',
+    'images/tn3.png',
+    'images/tn4.png'
+    ]
+});
+
+$('#host6').frameCarousel({
+  debug: false,
+  first: 0,
+  swipeThreshold: 10,
+  frame: 'images/frame.png',
+  frameSize:[
+        {width: 600, minScreenWidth: 1500},
+        {width: 500, minScreenWidth: 1200, maxScreenWidth: 1499},
+        {width: 450, minScreenWidth: 992, maxScreenWidth: 1199},
+        {width: 300, minScreenWidth: 768, maxScreenWidth: 991},
+        {width: 450, minScreenWidth: 521, maxScreenWidth: 767},
+        {width: 300, minScreenWidth: 369, maxScreenWidth: 520},
+        {width: 250, maxScreenWidth: 368}
+            ],
+  collapseThreshold: 320,
+  boundingBox: {
+    left: '4.3%',
+    top: '4.48%',
+    width: '91.4%',
+    height: '61.1%'
+  },
+  images: [
+    'images/tb1.png',
+    'images/tb2.png',
+    'images/tb3.png',
+    'images/tb4.png'
+    ]
+});
+
 var openmenubutton = $(".open-menu");
 var container = $(".st-container");
 
